ea49ac553cf5d789c800d3cd140a7db26d71f12e
juliahanssen <juliahanssen26@gmail.com>
2023-03-30 11:06:06 +0200

    la til hinder topp

--- Problemlosning/objektorienterte_prgrammer/Hoppespill2/hinder.py
+++ Problemlosning/objektorienterte_prgrammer/Hoppespill2/hinder.py
@@ -6,12 +6,15 @@ class Hinder(Figur):
     def __init__(self, x, y, fartX):
         super().__init__(x, y)
         self._fartX = fartX
-        self._hoyde = randint(20,600)
+        self._hoyde_bunn = randint(20,600)
+        self._hoyde_topp = randint(20,300)
         self._bredde = 90
-        self._y = 720 - self._hoyde
+        self._y = 720 - self._hoyde_topp
     
     def tegn_hinder(self, vindu):
-        pg.draw.rect(vindu, (180,40,70),(self._x, self._y,self._bredde, self._hoyde))
+        pg.draw.rect(vindu, (180,40,70),(self._x, 0,self._bredde, self._hoyde_topp))
+        pg.draw.rect(vindu, (180,40,70),(self._x, self._y,self._bredde, self._hoyde_bunn))
+
 
     def flytt_venstre(self):
         self._x -= 1
--- Problemlosning/objektorienterte_prgrammer/Hoppespill2/spiller.py
+++ Problemlosning/objektorienterte_prgrammer/Hoppespill2/spiller.py
@@ -5,15 +5,17 @@ class Spiller(Figur):
     def __init__(self, x ,y, fartY):
         super().__init__(x, y)
         self._fartY = fartY
+        self._radius = 25
 
     def tegn_spiller(self, vindu):
-        pg.draw.circle(vindu, (100,50,140), (self._x,self._y), 25)
+        pg.draw.circle(vindu, (100,50,140), (self._x,self._y), self._radius)
     
     def hopp(self):
         self._y -= 5
     
     def fall(self):
-        self._y +=1
-
+        if self._y < 720 - self._radius:
+             self._y +=1
+    
     def er_over_hinder():
         pass
